452e6d1e258a41ea943987c05dd22e4004dfb457
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-04-05 10:07:41 +0530

    added tests for or, nand, xor gates

--- simulator/spec/gates/orGate.spec.js
+++ simulator/spec/gates/orGate.spec.js
@@ -4,24 +4,21 @@
 
 import CodeMirror from 'codemirror';
 import { setup } from '../../src/setup';
-
 import load from '../../src/data/load';
-import circuitData from '../circuits/andGate.json';
+import circuitData from '../circuits/orGate.json';
 import testData from '../testData/gate.json';
 import { runAll } from '../../src/testbench';
 
 jest.mock('codemirror');
-
-describe('And Gate Testing', () => {
+describe('Or Gate Testing', () => {
     CodeMirror.fromTextArea.mockReturnValueOnce({ setValue: (text) => {} });
     setup();
-
     test('load circuitData', () => {
         expect(() => load(circuitData)).not.toThrow();
     });
     test('Run All tests', () => {
         load(circuitData);
-        const result = runAll(testData.AndGate, globalScope);
+        const result = runAll(testData.OrGate, globalScope);
         expect(result.summary.passed).toBe(4);
     });
 });
